e3d24ffe0cad825636f543f1bc77ad7856e9296c
matsair <matsschade@gmail.com>
2015-03-02 16:26:41 +0100

    Added Location Support

--- app/src/main/java/com/matsschade/weatherapp/MainActivity.java
+++ app/src/main/java/com/matsschade/weatherapp/MainActivity.java
@@ -1,19 +1,33 @@
 package com.matsschade.weatherapp;
 
+import android.location.Location;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    GoogleApiClient mClient;
+    Location mLastLocation;
+    TextView mLatitudeText;
+    TextView mLongitudeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        mLatitudeText = (TextView) findViewById(R.id.latitude);
+        mLongitudeText = (TextView) findViewById(R.id.longitude);
+        buildGoogleApiClient();
+
     }
 
 
@@ -38,4 +52,33 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    protected synchronized void buildGoogleApiClient() {
+        mClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mClient);
+        if (mLastLocation != null) {
+            mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
+            mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        //TODO
+    }
+
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        //TODO
+    }
+
+
 }
---
+++ app/src/main/java/com/matsschade/weatherapp/getData.java
@@ -0,0 +1,9 @@
+package com.matsschade.weatherapp;
+
+/**
+ * Created by Mats on 02/03/15.
+ */
+public class getData {
+
+    
+}
